f5d091b5f4a59cc9eaed9f54b056c7b6e37882ed
Flowrid <flowrid@outlook.com>
2026-06-13 14:24:49 +0800

    fix: 去除图片标签多余底色

--- components/HeroSearch.tsx
+++ components/HeroSearch.tsx
@@ -71,7 +71,7 @@ export default function HeroSearch() {
                     className="w-full h-full object-cover"
                     loading="eager"
                   />
-                  <div className="absolute bottom-0 left-0 right-0 text-white text-xs font-semibold text-center py-2">
+                  <div className="absolute bottom-0 left-0 right-0 text-white text-xs font-semibold text-center pb-2">
                     {img.alt}
                   </div>
                 </div>
